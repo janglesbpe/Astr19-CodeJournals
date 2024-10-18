eddcca0042b464d2128675ce23bfcebf8169d938
janglesbpe <jpeslami@ucsc.edu>
2024-10-18 01:32:03 -0700

    Create Journal 2.py

---
+++ Journal 2.py
@@ -0,0 +1,16 @@
+import numpy as np 
+#float point numbers
+x=19.8
+y=2.7
+#integers
+a=7
+b=4
+#arithmetic operators
+print("x+y=",x+y)
+print("type(x+y): ",type(x+y)) 
+
+print("b-a = ",b-a)              
+print("type(b-a): ",type(b-a))
+
+print("b*x = ",b*x)              
+print("type(b*x): ",type(b*x))
