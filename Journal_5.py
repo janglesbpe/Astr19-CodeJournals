fbe75fc27de273d9b720b5b1a3fdd1b8b1002a9d
janglesbpe <jpeslami@ucsc.edu>
2024-10-18 09:10:47 -0700

    Update Journal_5.py

--- Journal_5.py
+++ Journal_5.py
@@ -3,13 +3,13 @@ from astropy.table import Table
 
 #define main function
 def main():
-x=npy.linspace(1,2*npy.pi, 1000)
-y=np.sin(x)
-data_table= Table ()
-data_table["x"]=x
-data_table["y"]=y
+    x=npy.linspace(1,2*npy.pi, 1000)
+    y=np.sin(x)
+    data_table= Table()
+    data_table["x"]=x
+    data_table["y"]=y
+    print(data_table)
 
-print(data_table)
 #if/then
 if __name__ == '__main__':
     main()
