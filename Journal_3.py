1d1d36d40cae9cfc01c74e6e5e8dc684800e4c63
janglesbpe <jpeslami@ucsc.edu>
2024-10-18 01:47:05 -0700

    Create Journal_3.py

---
+++ Journal_3.py
@@ -0,0 +1,14 @@
+#define function
+def f(x):                
+    return x**3 + 8      
+
+#defining the integer
+def main():              
+    x = 9              
+    print("f(x) = x**3 + 8\n\nWhen x =",x,", f(x) =",f(x))
+    
+#final results
+    if f(x) > 27:
+        print("\nYAY!")        
+    else:
+        print("\nOh No!") 
