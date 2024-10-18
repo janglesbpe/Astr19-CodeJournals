531e637dd62e173d2b32ae536ef3d7df6816af07
janglesbpe <jpeslami@ucsc.edu>
2024-10-17 17:35:07 -0700

    journal input 1

---
+++ Journal_1.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+print('Hello World!')
+
+print("I\'m Blue Eslami\n My pronouns are They/Them.\n My favorite movie is Jurrassic World. I just love the dinosaurs.\n My favorite food is a nice IN-N-OUT.")
