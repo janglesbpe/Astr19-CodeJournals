1ed42c1d50595b8f7b021617e16e7ba8727a2a24
janglesbpe <jpeslami@ucsc.edu>
2024-10-18 02:12:13 -0700

    journal 4

---
+++ Journal_4.py
@@ -0,0 +1,24 @@
+#define main program
+def main():     
+#Classification/Parameter                 
+    BlueWhale = {                       
+        "name"      :    "BlueWhale",
+        "arm_L"     :    65.7     #inches
+        "leg_L"     :    0.0,   #inches
+        "eyes"      :    2,
+        "tail"      :    True,    
+        "furry"     :    False     
+    }
+
+#prints 
+    print("Favorite animal:",BlueWhale["name"])
+    print("\nArm length (in):",BlueWhale["arm_L"])
+    print("Leg length (in):",BlueWhale["leg_L"])
+    print("Number of eyes:",BlueWhale["eyes"])
+    print("Has tail:",BlueWhale["tail"])
+    print("Is furry:",BlueWhale["furry"])
+
+
+#final main function run
+if __name__ == "__main__":
+    main()
